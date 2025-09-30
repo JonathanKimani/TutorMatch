7464f1baceda438448cd4aff3a0e56ea24b8d250
JonathanKimani <kimtobena869@gmail.com>
2025-09-30 19:29:47 +0300

    fix: use import.meta.env for base path

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite' 
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -7,5 +7,5 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
-  base: import.meta.env.VITE_BASE_PATH || "/TutorMatch"
+  base: import.meta.env.VITE_BASE_PATH || "/",
 })
